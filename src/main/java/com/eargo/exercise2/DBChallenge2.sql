268a1aa4d82c91f8a1e2c089f42ecbf25c525953
durveshvedak <dvv223@nyu.edu>
2019-08-04 18:38:06 -0400

    Completed table transformation

--- src/main/java/com/eargo/exercise2/DBChallenge2.sql
+++ src/main/java/com/eargo/exercise2/DBChallenge2.sql
@@ -1,5 +1,7 @@
 create database eargo;
+
 use eargo;
+
 create table pages(id int not null auto_increment , home_page int, product_page int, warranty_page int, primary key (id) );
 
 insert into pages (home_page, product_page, warranty_page) values (1,1,1);
@@ -17,3 +19,7 @@ insert into pages (home_page, product_page, warranty_page) values (0,1,0);
 insert into pages (home_page, product_page, warranty_page) values (0,0,1);
 
 insert into pages (home_page, product_page, warranty_page) values (0,0,0);
+
+create table cols as (select column_name from information_schema.columns where table_name ='pages');
+
+create table page_count ( select column_name, count(*) from cols inner join pages group by column_name limit 5 offset 1 );
